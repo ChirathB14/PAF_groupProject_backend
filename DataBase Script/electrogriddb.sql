7bf27a648f2e50a8640bfd2f08d65f36c677ed76
ChirathB14 <chirathb14@gmail.com>
2022-04-25 14:46:14 +0530

    Modified Database script

--- DataBase Script/electrogriddb.sql
+++ DataBase Script/electrogriddb.sql
@@ -1,130 +1,142 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `electrogriddb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `electrogriddb`;
+-- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Apr 24, 2022 at 12:02 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: electrogriddb
+-- ------------------------------------------------------
+-- Server version	8.0.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `electrogriddb`
---
-CREATE DATABASE IF NOT EXISTS `electrogriddb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `electrogriddb`;
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `accounts`
 --
 
 DROP TABLE IF EXISTS `accounts`;
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `accountID` int(11) NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `accountID` int NOT NULL AUTO_INCREMENT,
   `city` varchar(20) NOT NULL,
   `buildingNo` varchar(10) NOT NULL,
   `totalAmtToPay` double NOT NULL,
-  `userID` int(11) NOT NULL,
-  PRIMARY KEY (`accountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  `userID` int DEFAULT NULL,
+  PRIMARY KEY (`accountID`),
+  KEY `acc_user_fk_id` (`userID`),
+  CONSTRAINT `acc_user_fk` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`accountID`, `city`, `buildingNo`, `totalAmtToPay`, `userID`) VALUES
-(1, 'Colobo', '754/A', 8500, 2),
-(2, 'Kandy', '45/B', 8745, 4);
-
--- --------------------------------------------------------
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'Colobo','754/A',8500,5),(2,'Kandy','45/B',8745,6);
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `bills`
 --
 
 DROP TABLE IF EXISTS `bills`;
-CREATE TABLE IF NOT EXISTS `bills` (
-  `billID` int(11) NOT NULL AUTO_INCREMENT,
-  `accountID` int(10) NOT NULL,
-  `unitUsage` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bills` (
+  `billID` int NOT NULL AUTO_INCREMENT,
+  `accountID` int DEFAULT NULL,
+  `unitUsage` int NOT NULL,
   `month` varchar(2) NOT NULL,
   `year` varchar(4) NOT NULL,
   `amount` double NOT NULL,
-  PRIMARY KEY (`billID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`billID`),
+  KEY `bill_acc_fk_idx` (`accountID`),
+  CONSTRAINT `bill_acc_fk` FOREIGN KEY (`accountID`) REFERENCES `accounts` (`accountID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `bills`
 --
 
-INSERT INTO `bills` (`billID`, `accountID`, `unitUsage`, `month`, `year`, `amount`) VALUES
-(1, 25, 95, '08', '2022', 750),
-(2, 12, 85, '05', '2022', 742);
-
--- --------------------------------------------------------
+LOCK TABLES `bills` WRITE;
+/*!40000 ALTER TABLE `bills` DISABLE KEYS */;
+INSERT INTO `bills` VALUES (1,1,95,'08','2022',750),(2,2,85,'05','2022',742);
+/*!40000 ALTER TABLE `bills` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `payments`
 --
 
 DROP TABLE IF EXISTS `payments`;
-CREATE TABLE IF NOT EXISTS `payments` (
-  `paymentID` int(11) NOT NULL AUTO_INCREMENT,
-  `accountID` int(11) NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `paymentID` int NOT NULL AUTO_INCREMENT,
+  `accountID` int DEFAULT NULL,
   `amount` double NOT NULL,
   `payMethod` varchar(10) NOT NULL,
   `payDate` varchar(15) NOT NULL,
-  PRIMARY KEY (`paymentID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`paymentID`),
+  KEY `pay_acc_fk_idx` (`accountID`),
+  CONSTRAINT `pay_acc_fk` FOREIGN KEY (`accountID`) REFERENCES `accounts` (`accountID`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `payments`
 --
 
-INSERT INTO `payments` (`paymentID`, `accountID`, `amount`, `payMethod`, `payDate`) VALUES
-(2, 2, 1000, 'card', '12/05/2022'),
-(3, 5, 1542, 'bank', '05/07/2022'),
-(4, 3, 9560, 'card', '05/12/2022');
-
--- --------------------------------------------------------
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+INSERT INTO `payments` VALUES (2,2,1000,'card','12/05/2022'),(3,1,1542,'bank','05/07/2022'),(4,2,9560,'card','05/12/2022');
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
   `userName` varchar(20) NOT NULL,
   `userNIC` varchar(20) NOT NULL,
   `userPhoneNo` varchar(12) NOT NULL,
   `userEmail` varchar(20) NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `userName`, `userNIC`, `userPhoneNo`, `userEmail`) VALUES
-(1, 'Kamal', '12457865', '0712345678', 'kamal@gmail.com'),
-(5, 'sriyani', '48795612345', '074568953', 'sriyani@gmail.com'),
-(6, 'kanthi', '4759561235', '072356985', 'kanthi@gmail.com'),
-(9, 'siripala', '4579953214', '0735689458', 'siripala@gmail.com'),
-(10, 'Amarapala', '1245967', '0124598', 'amarapala@gmail.com');
-COMMIT;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Kamal','12457865','0712345678','kamal@gmail.com'),(5,'sriyani','48795612345','074568953','sriyani@gmail.com'),(6,'kanthi','4759561235','072356985','kanthi@gmail.com'),(9,'siripala','4579953214','0735689458','siripala@gmail.com'),(10,'Amarapala','1245967','0124598','amarapala@gmail.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
